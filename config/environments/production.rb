886434263425e7c1008a5e59a05aeada68dafda9
tom96da <pascal.tom96@gmail.com>
2024-12-14 01:07:39 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
     "tom96da.com",     # Allow requests from example.com
-    /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+    /.*\.tom96da\.com/ # Allow requests from subdomains like `www.example.com`
   ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
